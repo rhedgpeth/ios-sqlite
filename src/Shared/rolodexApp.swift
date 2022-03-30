43a3071367cabfe30e6aa2cdbe8babdd3b7998bd
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2022-03-30 13:44:15 -0500

    Initial additions

---
+++ src/Shared/rolodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  rolodexApp.swift
+//  Shared
+//
+//  Created by Rob Hedgpeth on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct rolodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
